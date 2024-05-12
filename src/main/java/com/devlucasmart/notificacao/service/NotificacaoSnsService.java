608b0a46d1910eb7b5a392166a9498ca76b5f656
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-05-12 14:07:31 -0300

    refatora

--- src/main/java/com/devlucasmart/notificacao/service/NotificacaoSnsService.java
+++ src/main/java/com/devlucasmart/notificacao/service/NotificacaoSnsService.java
@@ -3,7 +3,6 @@ package com.devlucasmart.notificacao.service;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.model.PublishRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
